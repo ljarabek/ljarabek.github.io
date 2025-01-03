022bf87907dc2b02b867a1805781710e77336699
ljarabek <35137178+ljarabek@users.noreply.github.com>
2025-01-03 17:09:54 +0100

    Update config.jsx

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,21 +1,25 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Web app's Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCAYOYDuMKGGjTSJL5uDzG5hjQ6y_vYPiI",
-  authDomain: "auction-website-b12fc.firebaseapp.com",
-  databaseURL: "https://auction-website-b12fc.firebaseio.com",
-  projectId: "auction-website-b12fc",
-  storageBucket: "auction-website-b12fc.appspot.com",
-  messagingSenderId: "791747024664",
-  appId: "1:791747024664:web:215a222a81c6d0c2aeb06d",
+  apiKey: "AIzaSyCYTuJwU3lbppsc4xYUJuTMoWNUgxVXpU0",
+  authDomain: "drazbarripoff.firebaseapp.com",
+  projectId: "drazbarripoff",
+  storageBucket: "drazbarripoff.firebasestorage.app",
+  messagingSenderId: "372276741124",
+  appId: "1:372276741124:web:590c4fd36762447cdd2151",
+  measurementId: "G-WGQLS16XTG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Initialize Firestore and Auth
+// Initialize Firestore and Auth - TEGA NI BLO NOTR MOGOČE ZBRIŠEŠ???
 export const db = getFirestore(app);
 export const auth = getAuth(app);
